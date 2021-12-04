3871c99dd42f7c2567a74d986702e98b9d714335
Vechorkina <artdeco.t@i.ua>
2021-12-04 16:53:37 +0200

    Работают оба варианта
    Работают оба варианта

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,27 +1,26 @@
-//const selectedByClass = document.querySelectorAll('.item');
+const selectedByClass = document.querySelectorAll('.item');
         //const quantityСategories = selectedByClass.length;
         //console.log(`Number of  categories: ${quantityСategories.length}`);
-//console.log(`Number of  categories: ${selectedByClass.length}`);
-
-//selectedByClass.forEach(el => {
-  //const category = el.firstElementChild.textContent;
-  //const quantityElements = el.lastElementChild.children.length;
-  //console.log(`Category: ${category}`);
-  //console.log(`Elements: ${quantityElements}`,
-  //);
-//});
-
-
-const selectedByClass = document.querySelectorAll('.item');
-console.log(`Number of categories: ${selectedByClass.length}`);
+console.log(`Number of  categories: ${selectedByClass.length}`);
 
 selectedByClass.forEach(el => {
-    const numberOfCategory = el.querySelector('h2');
-    const numberOfElements = el.querySelectorAll('li');
-
-    console.log(`Category: ${numberOfCategory.textContent}`);
-    console.log(`Elements: ${numberOfElements.length}`);
+  const category = el.firstElementChild.textContent;
+  const quantityElements = el.lastElementChild.children.length;
+  console.log(`Category: ${category}`);
+  console.log(`Elements: ${quantityElements}`,
+  );
 });
+
+
+//const selectedByClass = document.querySelectorAll('.item');
+//console.log(`Number of categories: ${selectedByClass.length}`);
+//selectedByClass.forEach(el => {
+    //const numberOfCategory = el.querySelector('h2');
+    //const numberOfElements = el.querySelectorAll('li');
+
+    //console.log(`Category: ${numberOfCategory.textContent}`);
+    //console.log(`Elements: ${numberOfElements.length}`);
+//});
 
 
 
