f56e38990197022261790d067f831bf52a6f66bf
Vechorkina <artdeco.t@i.ua>
2021-12-06 19:42:47 +0200

    Добавляет описание заданий
    Описание заданий

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,20 @@
+/* В HTML есть список категорий ul#categories.
+Напиши скрипт который:
+Посчитает и выведет в консоль количество категорий в ul#categories,
+то есть элементов li.item.
+Для каждого элемента li.item в списке ul#categories, найдет и выведет 
+в консоль текст заголовка элемента (тега <h2>) и 
+  количество элементов в категории (всех вложенных в него <li>).
+В результате, в консоли будут выведены такие сообщения.
+
+Number of categories: 3
+Category: Animals
+Elements: 4
+Category: Products
+Elements: 3
+Category: Technologies
+Elements: 5 */
+
 const selectedByClass = document.querySelectorAll('.item');
         //const quantityСategories = selectedByClass.length;
         //console.log(`Number of  categories: ${quantityСategories.length}`);
