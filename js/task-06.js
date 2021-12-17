f3acdd4b2c1502fb86c19db09f6009ccef15809d
Vechorkina <artdeco.t@i.ua>
2021-12-17 19:27:00 +0200

    Task - 06
    Task - 06

--- js/task-06.js
+++ js/task-06.js
@@ -21,15 +21,15 @@
  */
 
 const inputEl = document.getElementById('validation-input');
-/* console.log(inputEl);
+ console.log(inputEl);
 const lengthInputEl = inputEl.dataset.length;
-console.log(lengthInputEl); */
+console.log(lengthInputEl);
 inputEl.addEventListener('focus', onFocusInput);
 inputEl.addEventListener('blur', onBlurInput);
 function onFocusInput(event) {
 }
 function onBlurInput(event) {
-   if(event.currentTarget.value.length === 6){
+   if(event.currentTarget.value.length === Number(lengthInputEl)){
       event.currentTarget.classList.remove('invalid');
       event.currentTarget.classList.add('valid');
    } else {
